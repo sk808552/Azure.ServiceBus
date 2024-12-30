74d00375df9c567754870e2f2a707f3aecdb60fd
Prasanta Kumar Khuntia <sk808552@gmail.com>
2024-12-30 22:55:46 +0530

    Update Program.cs

--- AzureServiceBusSender/Program.cs
+++ AzureServiceBusSender/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Azure.Messaging.ServiceBus;
+
+var connectionString = "";
+var queueName = "az-queue-pras";
+var maxNoMsg = 1;
+
+ServiceBusClient serviceBusClient = new ServiceBusClient(connectionString);
+ServiceBusSender serviceBusSender = serviceBusClient.CreateSender(queueName);
+ServiceBusMessageBatch batch = await serviceBusSender.CreateMessageBatchAsync();
+
+for (int i = 0; i < maxNoMsg; i++)
+{
+    if (!batch.TryAddMessage(new ServiceBusMessage("This is message no " + i)))
+    {
+        Console.WriteLine("Message " + i + " didn't added to the batch");
+    }
+}
+
+try
+{
+    await serviceBusSender.SendMessageAsync(new ServiceBusMessage(batch.ToString()));
+    Console.WriteLine("Messages Sent Successfully");
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.ToString());
+}
+finally
+{
+    await serviceBusClient.DisposeAsync();
+    await serviceBusSender.DisposeAsync();
+}
